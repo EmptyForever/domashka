7655a52fab5a1e4d32e6deb5973b509731d84d89
Aleks <kakdela208@mail.ru>
2022-11-27 23:19:06 +1000

    complete

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//берём два числа от пользователя, сравниваем их и выводим максимальное
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (number1 > number2)
+{
+    max = number1;
+    Console.WriteLine($"Максимальное число {max}");
+} 
+else if (number1 < number2) {
+    max = number2;
+    Console.WriteLine($"Максимальное число {max}");
+} else {
+    Console.WriteLine("Эти числа равны");
+}
