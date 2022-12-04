f18d16bd1936c34736e2369aaf0a4cf8593daef9
Aleks <kakdela208@mail.ru>
2022-12-04 22:07:25 +1000

    complete task23

--- DZ_3_seminars/Task23/Program.cs
+++ DZ_3_seminars/Task23/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+
+// берём число пользователя. целое, не меньше 1 (то есть >= 1 или !< 1)
+// Возводим первое число в куб, распечатываем число и результат. прибавляем к счётчику 1
+// пока счётчик (=1) не станет больше числа(N) выолняем это действие!
+
+Console.Clear();
+int numUser = -1;
+while (numUser < 1)
+{
+    Console.WriteLine("Введите натуральное число: ");
+    numUser = Convert.ToInt32(Console.ReadLine());
+    if (numUser < 1) Console.WriteLine("Введены неверные данные: ");
+}
+
+string ThirdDegree(int number)
+{
+    int count = 1;
+    string table = "";
+    while (count <= number)
+    {
+        table = table + $"{count} | {count * count * count} \n";
+        count++;
+    }
+    return table;
+}
+
+Console.WriteLine($"Таблица кубов цифр от 1 до {numUser} \n{ThirdDegree(numUser)}");
