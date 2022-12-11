8d6b3cd9b1d4899c930018e12a5fbf91de526599
Aleks <kakdela208@mail.ru>
2022-12-11 16:38:29 +1000

    add task 24 26  and 28

---
+++ DZ_5_seminars/Task38/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// создание вещественного массива СО случайными числами
+double[] CreateArrayRndDouble(int count, int min, int max)
+{
+    double[] array = new double[count];
+    int index = 0;
+    Random rnd = new Random();
+    while (index < array.Length)
+    {
+        array[index] = rnd.NextDouble() * (max - min) + min;
+        index++;
+    }
+    return array;
+}
+
+// печать массива
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    int last = 0;
+    if (array.Length > 1)
+    {
+        for (int i = 0; i <= array.Length - 2; i++)
+        {
+            Console.Write($"{array[i]}, ");
+            last = i;
+        }
+        last = last + 1;
+        Console.Write(array[last]);
+    }
+    else Console.Write(array[last]);
+    Console.Write("]");
+}
+
+PrintArray(CreateArrayRndDouble(10, 0, 5));
