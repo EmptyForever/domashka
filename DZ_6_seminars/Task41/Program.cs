07801972e8cf463e8be5de66b0014469db6ce2db
Aleks <kakdela208@mail.ru>
2022-12-12 21:00:41 +1000

    commit task complete

--- DZ_6_seminars/Task41/Program.cs
+++ DZ_6_seminars/Task41/Program.cs
@@ -1,2 +1,56 @@
-﻿double res = c + a/b;
-// a/b --> если int a и b , то a/b тоже будет int,  а не 2,5 --> 2
+﻿// \\====== пользователь вводит с клавиатуру M чисел. Сколько > 0 ?
+// -1, -7, 567, 89, 223-> 3
+
+// 2. через массив
+// 2.1 пользователь задаёт число M - и тогда мы знаем размер будущего массива
+// 2.2 создаём массив из M кол-ва элементов и присваиваем последовательно всем элементам значения
+// до тех пор пока i < array.Length
+
+
+// создаём новый массив. size --> Определяет пользователь размер массива. array[i] --> тоже задаёт User
+int[] CreateArrayUser(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+
+
+// метод для опеределения кол-ва элементов > 0
+int CountMoreZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) if (array[i] > 0) count++;
+    return count;
+}
+
+
+// печать массива
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    int last = 0;
+    if (array.Length > 1)
+    {
+        for (int i = 0; i <= array.Length - 2; i++)
+        {
+            Console.Write($"{array[i]}, ");
+            last = i;
+        }
+        last = last + 1;
+        Console.Write(array[last]);
+    }
+    else Console.Write(array[last]);
+    Console.Write("]");
+}
+
+
+Console.Clear();
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] arrayNew = CreateArrayUser(size);
+int count = CountMoreZero(arrayNew);
+PrintArray(arrayNew);
+Console.WriteLine($"\nКол-во элементов > 0  -->  {count}");
--- DZ_6_seminars/Task43/Program.cs
+++ DZ_6_seminars/Task43/Program.cs
@@ -15,14 +15,32 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("b2: ");
 double b2 = Convert.ToInt32(Console.ReadLine());
 
+
 // метод для нахождения точки пересечения двух прямых. Аргументы - k иксов и c 
-double[] PeresecLine(double k1, double b1, double k2, double b2)
+double[] CrossPoint(double k1, double b1, double k2, double b2)
 {
+    double x = default;
     double y = (k1 * b2 - k2 * b1) / (k1 - k2);
-    double x = (y - b1) / k1;
+// для k1 == 0 мы просто пользуемся другой кусочкек системы неравенств
+    if (k1 == 0) 
+    {
+        x = (y - b2) / k2;
+    }
+    else 
+    {
+        x = (y - b1) / k1;
+    }
     return new double[2] { x, y };
 }
 
-double[] array = PeresecLine(k1, b1, k2, b2);
 
-Console.WriteLine($"( {array[0]}; {array[1]} )");
+if (k1 == k2)
+{
+    if (b1 == b2) Console.WriteLine("Заданные прямые совпадают");
+    else Console.WriteLine("Заданные прямые параллельны и никогда не пересекаются");
+}
+else 
+{
+    double[] array = CrossPoint(k1, b1, k2, b2);
+    Console.WriteLine($"\nk1 = {k1}, b2 = {b1}, k2 = {k2}, b2 = {b2}  \nКоординаты x и y точки пересечения --> ( {array[0]}; {array[1]} )");
+}
